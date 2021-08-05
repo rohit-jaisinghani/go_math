6d8b8ffde41bc79c6549d3fadf67b517a6163386
rohit <someone@someplace.com>
2021-08-05 11:38:59 +0530

    error condition is added

--- calc/calc.go
+++ calc/calc.go
@@ -1,5 +1,12 @@
 package calc
 
-func Add(a,b int) int{
-	return a+b
+import "errors"
+
+func Add(a,b int) (int,error){
+	if a!=0{
+		return a+b,nil
+	}else{
+		return 0,errors.New("value of a is zero")
+	}
+
 }
